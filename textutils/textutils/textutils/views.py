19a035530384828116b97bb39b8714a1c3b05d7b
Meetpatel2306 <140637154+Meetpatel2306@users.noreply.github.com>
2024-04-30 21:38:12 +0530

    views.py

--- textutils/textutils/textutils/views.py
+++ textutils/textutils/textutils/views.py
@@ -1,61 +1,9 @@
-# from django.http import HttpResponse
-# from django.shortcuts import render
-#
-# # # video 9 text box banavanu and button tag and button dabao to charcounter page khule
-# def index(request):
-#     return render(request,'index.html')
-
-
-# video 9
-# def charcounter(request):
-#     print(request.GET.get('text','default'))#text=text mate ,default=kai na lakhie to default lai le
-#     return HttpResponse('''charcounter <br><a href="http://127.0.0.1:8000/"> back to home</a>''')
-
-# templates and index page with adding parameters video 8
-# def index(request):
-#     a={'name':"meet patel",'place':"ahmedabad",'age':20 }
-#     return render(request,'index.html',a)
-
-# code for video 6
-# def index(request):
-#     #youtube video access link
-#     return HttpResponse('''<h1> meet patel </h1> <a href="https://youtu.be/AepgWsROO4k?si=WgRO23DcMnxvnaJv">Django code with harry video 6 is here</a>''' )
-# def about(request):
-#     return HttpResponse("my name is meet patel")
-
-# back button video 7
-# def index(request):
-#     return HttpResponse("home page this is meet patel ")
-# def removepunc(request):
-#     return HttpResponse('''remove punc page <br><a href="http://127.0.0.1:8000/"> back to home</a>''')
-# def capitalizefirst(request):
-#     return HttpResponse('''capital first<br><a href="http://127.0.0.1:8000/"> back to home</a>''')
-# def newlineremove(request):
-#     return HttpResponse('''new line remover page <br><a href="http://127.0.0.1:8000/">back to home </a>''' )
-# def spaceremover(request):
-#     return HttpResponse('''space remover<br><a href="http://127.0.0.1:8000/"> back to home</a>''')
-# def charcounter(request):
-#     return HttpResponse('''charcounter <br><a href="http://127.0.0.1:8000/"> back to home</a>''')
-
-
 from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
 
-# def exercise1(request):
-#     a='''<h1>Navigation Bar <br></h1>
-#             <a href="https://www.flipkart.com/">Flipkart</a>
-#             <br>
-#             <a href="https://www.instagram.com/">instagram</a>
-#             <br>
-#             <a href="https://www.facebook.com/">facebook</a>
-#             <br>
-#             <a href="https://www.whatsapp.com/">whatsapp</a>'''
-#     return HttpResponse(a)
-
-# video 10
 def analyze(request):
     # Get the text
     djtext = request.POST.get('text', 'default')
