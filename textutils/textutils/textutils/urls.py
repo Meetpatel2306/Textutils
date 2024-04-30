bf49de2ad0cf75de8d66546c0b14f307f75ae5c9
Meetpatel2306 <140637154+Meetpatel2306@users.noreply.github.com>
2024-04-30 21:37:11 +0530

    urls.py

--- textutils/textutils/textutils/urls.py
+++ textutils/textutils/textutils/urls.py
@@ -14,33 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-# from django.urls import path
-# from . import views
 
-# code for video 6
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('',views.index,name='index'),
-#     path('about',views.about,name='about')
-# ]
 
-# code for video 7
-# urlpatterns = [
-#      path('admin/', admin.site.urls),
-#      path('',views.index,name='index'),
-
-    #  video 7
-    #  path('removepunc',views.removepunc,name='removep'),
-    #  path('capitalizefirst',views.capitalizefirst,name='capfirst'),
-#     #  path('newlineremove', views.newlineremove, name='newlineremove'),
-#     #  path('spaceremover', views.spaceremover, name='spaceremover'),
-#     # path('charcounter',views.charcounter,name='charcounter'),
-#
-# # video 9 text box banavanu and button tag and button dabao to charcounter page khule
-#      path('charcounter',views.charcounter,name='charcounter'),
-
-# video 10
 from django.contrib import admin
 from django.urls import path
 from . import views
@@ -49,5 +24,4 @@ urlpatterns = [
    path('admin/', admin.site.urls),
    path('', views.index, name='index'),
    path('analyze', views.analyze, name='analyze'),
-   # path('exercise1',views.exercise1,name='exercise1')
 ]
